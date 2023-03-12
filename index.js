9b09de28eae7c05e72632b7dd6aaa15b7eadb320
Abdurakhmon Ibragimov <abdurakhmon.ibragimov.006@gmail.com>
2023-03-12 12:08:58 +0500

    First Project

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const btn2 = document.querySelector(".increment");
 const num = document.querySelector("#counter");
 
 btn1.addEventListener("click", () => {
-  num.innerText =  +num.textContent - 1;
+  num.innerText = +num.textContent - 1;
 });
 btn2.addEventListener("click", () => {
-  num.innerText =  +num.textContent + 1;
+  num.innerText = +num.textContent + 1;
 });
