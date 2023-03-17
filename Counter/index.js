034df2fde51965dc746d9ae86be8c184b6885216
Abdurakhmon Ibragimov <Abdurakhmon.ibragimov.006@gmail.com>
2023-03-17 20:09:26 +0500

    Counter

---
+++ Counter/index.js
@@ -0,0 +1,10 @@
+const btn1 = document.querySelector(".decrement");
+const btn2 = document.querySelector(".increment");
+const num = document.querySelector("#counter");
+
+btn1.addEventListener("click", () => {
+  num.innerText = +num.textContent - 1;
+});
+btn2.addEventListener("click", () => {
+  num.innerText = +num.textContent + 1;
+});
